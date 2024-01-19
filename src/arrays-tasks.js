e0a537764c4534147daebef282e0187f2ce0104a
Vitali Klepcha <aphelionwork@gmail.com>
2024-01-19 11:43:15 +0100

    feat: 13 tasks were completed

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -94,8 +94,8 @@ function findAllOccurrences(arr, item) {
  *    removeFalsyValues([ 1, 2, 3, 4, 5, 'false' ]) => [ 1, 2, 3, 4, 5, 'false' ]
  *    removeFalsyValues([ false, 0, NaN, '', undefined ]) => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues(arr) {
+  return arr.filter(Boolean);
 }
 
 /**
@@ -108,8 +108,11 @@ function removeFalsyValues(/* arr */) {
  *    getStringsLength([ '', 'a', 'bc', 'def', 'ghij' ]) => [ 0, 1, 2, 3, 4 ]
  *    getStringsLength([ 'angular', 'react', 'ember' ]) => [ 7, 5, 5 ]
  */
-function getStringsLength(/* arr */) {
-  throw new Error('Not implemented');
+function getStringsLength(arr) {
+  const nameLenght = arr.map(function (name) {
+    return name.length;
+  });
+  return nameLenght;
 }
 
 /**
@@ -126,8 +129,14 @@ function getStringsLength(/* arr */) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length > 0) {
+    const value = arr.reduce((sum, current) => sum + current, 0);
+    const temp = value / arr.length;
+    const temp1 = temp.toFixed(2);
+    return Number(temp1);
+  }
+  return 0;
 }
 
 /**
